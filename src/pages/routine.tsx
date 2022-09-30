3dccb83214103ef751074fb548acec0c1066c739
seoyoonyi <yiseoyoon@gmail.com>
2022-09-30 12:53:07 +0900

    오류해결, props 타입 오류 해결중

--- src/pages/routine.tsx
+++ src/pages/routine.tsx
@@ -1,18 +1,12 @@
-import React, {
-  useState,
-  useContext,
-  useEffect,
-  PropsWithChildren,
-} from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Btn from '../components/btn';
-
 import { IDataType, RoutineStateContext } from '../context/RoutineStateContext';
 interface IRoutineProps {
   closeRoutine?: () => void;
 }
 
-const Routine = ({ closeRoutine }: PropsWithChildren<IRoutineProps>) => {
+const Routine = ({ closeRoutine }: IRoutineProps) => {
   let { id } = useParams();
   const routineList = useContext(RoutineStateContext);
   const navigate = useNavigate();
@@ -27,24 +21,23 @@ const Routine = ({ closeRoutine }: PropsWithChildren<IRoutineProps>) => {
       if (targetRoutine) {
         setData(targetRoutine);
       } else {
-        alert('없는 루틴입니다.');
         navigate('/', { replace: true });
+        alert('없는 루틴입니다.');
       }
     }
   }, [id, routineList]);
 
   if (!data) {
     return <div>로딩중입니다...</div>;
-  } else {
-    return (
-      <div>
-        <h3>{data.title}</h3>
-        <p>{data.content}</p>
-        <p>{data.date}</p>
-        <Btn onClick={closeRoutine} text={'X'} />
-      </div>
-    );
   }
+  return (
+    <div>
+      <h3>{data.title}</h3>
+      <p>{data.content}</p>
+      <p>{data.date}</p>
+      <Btn onClick={closeRoutine} text={'X'} />
+    </div>
+  );
 };
 
 export default Routine;
