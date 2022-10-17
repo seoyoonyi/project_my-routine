69e9a6700c7600ac288dae9ff790b6b6e775d7cc
seoyoonyi <yiseoyoon@gmail.com>
2022-10-17 18:29:50 +0900

    axios 로직정리, 서버delete 수정

--- src/routines/routines.module.ts
+++ src/routines/routines.module.ts
@@ -1,13 +1,11 @@
-import { Module } from '@nestjs/common';
-import { RoutinesService } from './routines.service';
-import { RoutineController } from './routines.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Routine } from './entitles/routine.entity';
-import { RoutinesRepository } from './routines.repository';
-
+import { Module } from '@nestjs/common'
+import { RoutinesService } from './routines.service'
+import { RoutineController } from './routines.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Routine } from './entitles/routine.entity'
 @Module({
   imports: [TypeOrmModule.forFeature([Routine])],
   controllers: [RoutineController],
-  providers: [RoutinesService, RoutinesRepository],
+  providers: [RoutinesService],
 })
 export class RoutineModule {}
--- src/routines/routines.service.ts
+++ src/routines/routines.service.ts
@@ -1,29 +1,46 @@
-import { Injectable } from '@nestjs/common';
-import { CreateRoutineDto } from './dto/create-routine.dto';
-import { UpdateRoutineDto } from './dto/update-routine.dto';
-import { RoutinesRepository } from './routines.repository';
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { CreateRoutineDto } from './dto/create-routine.dto'
+import { UpdateRoutineDto } from './dto/update-routine.dto'
+import { Routine } from './entitles/routine.entity'
 
 @Injectable()
 export class RoutinesService {
-  constructor(private readonly repo: RoutinesRepository) {}
+  constructor(@InjectRepository(Routine) private readonly repo: Repository<Routine>) {}
 
-  async create(createRoutineDto: CreateRoutineDto) {
-    return this.repo.create(createRoutineDto);
+  async create(createRoutineDto: CreateRoutineDto): Promise<Routine> {
+    const routine = this.repo.create(createRoutineDto)
+    return this.repo.save(routine)
   }
 
   async findAll() {
-    return this.repo.findAll();
+    return this.repo.find()
   }
 
   async findOneBy(id: number) {
-    return this.repo.findOneBy(id);
+    return this.repo.findOneBy({ id })
   }
 
   async update(id: number, updateRoutineDto: UpdateRoutineDto) {
-    return this.repo.update(id, updateRoutineDto);
+    return this.repo.update(id, updateRoutineDto)
+  }
+
+  // sqlite에서 삭제된 데이터의 시퀀스가 남아있어 AUTO_INCREMENT 사용시 인덱스가 초기화가 안되는 문제 해결
+  resetSequence = async () => {
+    const reponse = await this.findAll()
+    if (reponse?.length === 0) {
+      await this.repo.query('UPDATE SQLITE_SEQUENCE SET seq = 0 WHERE name = "product"')
+    }
   }
 
   async delete(id: number) {
-    return this.repo.delete(id);
+    const excute = this.repo.delete(id)
+
+    excute.then(() => {
+      this.resetSequence()
+    })
+
+    return excute
   }
 }
