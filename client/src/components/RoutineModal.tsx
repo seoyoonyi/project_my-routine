c2b9a70cfb6313f1cd522d763421eae7e9f9b291
seoyoonyi <yiseoyoon@gmail.com>
2022-10-18 02:28:22 +0900

    edit기능 구현

--- client/src/components/RoutineModal.tsx
+++ client/src/components/RoutineModal.tsx
@@ -2,6 +2,7 @@ import { Modal, Dropdown, Menu } from 'antd';
 import type { MenuProps } from 'antd';
 import { useState } from 'react';
 import { IRoutineListProps } from './RoutineList';
+import Btn from './Btn';
 
 interface IRoutineModal {
   isModalOpen: boolean;
@@ -12,16 +13,46 @@ interface IRoutineModal {
 const RoutineModal = ({ isModalOpen, routineItem, handleCancel }: IRoutineModal) => {
   const { id, title, content, date, routineController, getRoutinesData } = routineItem;
   const [menuKey, setMenuKey] = useState<string>('');
-
-  const handleMenuClick: MenuProps['onClick'] = (e) => {
-    const dropDownId = e.key;
-    setMenuKey(dropDownId);
+  const [originData, setOriginData] = useState({ title, content, date });
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [isEditDate, setIsEditDate] = useState<boolean>(false);
+  //TODO: 수정기능 오빠한테 물어보기
+  //수정 기능 - 공통
+  const handleChangeEdit = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.currentTarget;
+    setOriginData({
+      ...originData,
+      [name]: value,
+    });
+  };
+  //수정 기능 - 타이틀, 콘텐츠
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+  const handleQuitEdit = () => {
+    setIsEdit(false);
+    setOriginData({ title, content, date });
+  };
+  const EditRoutineData = async () => {
+    await routineController.editRoutine(id, originData.title, originData.content, originData.date);
+    toggleIsEdit();
+    getRoutinesData();
+  };
+  //수정기능 - 날짜
+  const toggleIsEditDate = () => setIsEditDate(!isEditDate);
+  const EditRoutineDateData = async () => {
+    await routineController.editRoutine(id, originData.title, originData.content, originData.date);
+    toggleIsEditDate();
+    getRoutinesData();
   };
 
+  //삭제 기능
   const removeRoutineData = async () => {
     await routineController.removeRoutine(id);
     handleCancel();
     getRoutinesData();
+  };
+  const handleMenuClick: MenuProps['onClick'] = (e) => {
+    const dropDownId = e.key;
+    setMenuKey(dropDownId);
   };
 
   const menu = (
@@ -51,9 +82,29 @@ const RoutineModal = ({ isModalOpen, routineItem, handleCancel }: IRoutineModal)
   return (
     <>
       <Modal title={[dropDown]} open={isModalOpen} onCancel={handleCancel} footer={null}>
-        <h2>{title}</h2>
-        <p>{content}</p>
-        <p>{date}</p>
+        {isEdit ? (
+          <>
+            <input name="title" defaultValue={title} onChange={handleChangeEdit} />
+            <br />
+            <textarea name="content" defaultValue={content} onChange={handleChangeEdit}></textarea>
+            <br />
+            <Btn onClick={handleQuitEdit}>취소</Btn>
+            <Btn onClick={EditRoutineData}>저장</Btn>
+          </>
+        ) : (
+          <>
+            <h2 onClick={toggleIsEdit}>{title}</h2>
+            <p onClick={toggleIsEdit}>{content}</p>
+          </>
+        )}
+        {isEditDate ? (
+          <>
+            <input type="date" name="date" onChange={handleChangeEdit} defaultValue={date} />
+            <Btn onClick={EditRoutineDateData}>저장</Btn>
+          </>
+        ) : (
+          <p onClick={toggleIsEditDate}>{originData.date}</p>
+        )}
       </Modal>
     </>
   );
