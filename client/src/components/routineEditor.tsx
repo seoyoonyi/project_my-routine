f3f483f90e3dc4fab634dc0cbd6f754226b57f8b
seoyoonyi <yiseoyoon@gmail.com>
2022-10-19 17:20:14 +0900

    eslintrc 기능설정, 날짜기능 구현

--- client/src/components/routineEditor.tsx
+++ client/src/components/routineEditor.tsx
@@ -2,72 +2,98 @@ import React, { useRef, useState } from 'react';
 import Btn from './Btn';
 import RoutineClient from '../service/routine-client';
 import { getStringDate } from '../common/utils';
+import { CalendarOutlined } from '@ant-design/icons';
 
 interface IRoutineEditorProps {
-  getRoutinesData: () => void;
-  routineToggle: () => void;
-  routineController: RoutineClient;
+	getRoutinesData: () => void;
+	routineToggle: () => void;
+	routineController: RoutineClient;
 }
 
 export interface IRoutineDataType {
-  title: string;
-  content: string;
-  date: string;
+	title: string;
+	content: string;
+	date: string;
 }
 
-const RoutineEditor = ({ getRoutinesData, routineToggle, routineController }: IRoutineEditorProps) => {
-  const [routineData, setRoutineData] = useState<IRoutineDataType>({
-    title: '',
-    content: '',
-    date: getStringDate(new Date()),
-  });
-  const [onDate, setOnDate] = useState(false);
-  const titleInput = useRef<HTMLInputElement>(null);
-  const contentInput = useRef<HTMLTextAreaElement>(null);
+const RoutineEditor = ({
+	getRoutinesData,
+	routineToggle,
+	routineController,
+}: IRoutineEditorProps) => {
+	const [routineData, setRoutineData] = useState<IRoutineDataType>({
+		title: '',
+		content: '',
+		date: getStringDate(new Date()),
+	});
+	const [onDate, setOnDate] = useState(false);
+	const titleInput = useRef<HTMLInputElement>(null);
+	const contentInput = useRef<HTMLTextAreaElement>(null);
 
-  const dateToggle = () => {
-    setOnDate((onDate) => !onDate);
-  };
+	const dateToggle = () => {
+		setOnDate(onDate => !onDate);
+	};
 
-  const routineSave = () => routineToggle();
+	const routineSave = () => routineToggle();
 
-  const handleChangeRoutine = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setRoutineData({
-      ...routineData,
-      [e.target.name]: e.target.value,
-    });
-  };
+	const handleChangeRoutine = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		setRoutineData({
+			...routineData,
+			[e.target.name]: e.target.value,
+		});
+	};
 
-  const addRoutineData = async () => {
-    await routineController.addRoutine(routineData.title, routineData.content, routineData.date);
-    getRoutinesData();
-  };
+	const addRoutineData = async () => {
+		await routineController.addRoutine(routineData.title, routineData.content, routineData.date);
+		getRoutinesData();
+	};
 
-  const handleSubmit = () => {
-    addRoutineData();
-    setRoutineData({
-      title: '',
-      content: '',
-      date: getStringDate(new Date()),
-    });
-    routineSave();
-  };
+	const handleSubmit = () => {
+		addRoutineData();
+		setRoutineData({
+			title: '',
+			content: '',
+			date: getStringDate(new Date()),
+		});
+		routineSave();
+	};
 
-  return (
-    <>
-      <input ref={titleInput} name="title" value={routineData.title} onChange={handleChangeRoutine} />
-      <br />
+	return (
+		<>
+			<input
+				ref={titleInput}
+				name="title"
+				value={routineData.title}
+				onChange={handleChangeRoutine}
+			/>
+			<br />
 
-      <textarea ref={contentInput} name="content" value={routineData.content} onChange={handleChangeRoutine}></textarea>
-      <br />
-      <div>
-        <Btn onClick={dateToggle}>오늘</Btn>
+			<textarea
+				ref={contentInput}
+				name="content"
+				value={routineData.content}
+				onChange={handleChangeRoutine}
+			/>
+			<br />
+			<div>
+				<Btn onClick={dateToggle}>오늘</Btn>
 
-        {onDate ? <input type="date" name="date" onChange={handleChangeRoutine} value={routineData.date} /> : null}
-        <Btn onClick={handleSubmit}>루틴저장</Btn>
-      </div>
-    </>
-  );
+				{onDate ? (
+					<>
+						{/* TODO: 앧트디자인 컴포넌트 가져다 쓰기 */}
+						{/* <Input
+							type="date"
+							name="date"
+							onChange={handleChangeRoutine}
+							value={routineData.date}
+						/> */}
+						<CalendarOutlined />
+					</>
+				) : null}
+				<Btn onClick={handleSubmit}>루틴저장</Btn>
+			</div>
+		</>
+	);
 };
 
 export default RoutineEditor;
