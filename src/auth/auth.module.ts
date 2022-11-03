843dca5f37befead8749b9ede39478c310448524
happytwo1101 <sh.yoon@purple.io>
2022-11-03 12:52:27 +0900

    add 로그인 및 swagger 보안 강화

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { forwardRef, Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { TypeOrmModule } from "@nestjs/typeorm";
@@ -9,10 +10,11 @@ import { JwtStrategy } from "./jwt/jwt.strategy";
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     PassportModule.register({ defaultStrategy: "jwt", session: false }),
 
     JwtModule.register({
-      secret: "secret",
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: "1y" },
     }),
 
