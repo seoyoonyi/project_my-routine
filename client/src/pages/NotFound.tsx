5ae5d70cec4feb851067c555ee844622274f764a
seoyoonyi <yiseoyoon@gmail.com>
2022-12-10 00:27:03 +0900

    [Add] NotFound 페이지 CSS 추가

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,43 +1,32 @@
-import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Header from '../components/Header';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import MainContainer from '../components/MainContainer';
+import styles from './NotFound.module.css';
+import Btn from '../components/Btn';
+import { AlertCircle } from 'lucide-react';
 
 const NotFound = () => {
-	//404 page 가로스크롤 생김
+	const navigate = useNavigate();
+
+	const goToHome = (e: { preventDefault: () => void }) => {
+		e.preventDefault();
+		navigate('/');
+	};
 	return (
 		<>
 			<Header />
-			<MainContainer>
-				<div className="error-wrap">
-					<div className="container">
-						<div className="error-con-group">
-							<div className="error-icon-group">
-								<FontAwesomeIcon
-									icon={faExclamationCircle}
-									color="#f3807c"
-									className="error-icon"
-								/>
-								<h2 className="error-icon-404">404</h2>
-							</div>
-
-							<h2 className="error-tit">요청하신 페이지를 찾을 수 없습니다.</h2>
-							<p className="error-desc">
-								불편을 드려 죄송합니다.<br></br>확인 후 다시 시도해주세요.
-							</p>
-							<div className="error-btn">
-								<Link to="/">홈으로 가기</Link>
-								{/* <button onClick={goToHome} className="error-home-btn btn-main btn-40">
-										홈으로 가기
-									</button>
-									<button onClick={goToBack} className="gray-btn-outlined btn-40">
-										뒤로가기
-									</button> */}
-							</div>
-						</div>
-					</div>
+			<MainContainer className="text-center">
+				<div className={styles.errorIconGroup}>
+					<AlertCircle size={80} color="#37e2d5" />
+					<h2 className={styles.errorIcon404}>404</h2>
 				</div>
+
+				<h2 className={styles.errorTit}>요청하신 페이지를 찾을 수 없습니다.</h2>
+				<p className={styles.errorDesc}>불편을 드려 죄송합니다. 확인 후 다시 시도해주세요.</p>
+
+				<Btn onClick={goToHome} className={styles.errorBtn} size="large">
+					홈으로 가기
+				</Btn>
 			</MainContainer>
 		</>
 	);
