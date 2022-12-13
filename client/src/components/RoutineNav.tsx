7f0e70b5183915c110b67bbde7b8689795f309a1
seoyoonyi <yiseoyoon@gmail.com>
2022-12-13 21:15:10 +0900

    fit: 버그수정

--- client/src/components/RoutineNav.tsx
+++ client/src/components/RoutineNav.tsx
@@ -29,22 +29,33 @@ const RoutineNav = ({ changeWeek, onAdd, routineToggle, ActiveStatusTolggle }: I
 			<div className="flex items-center justify-between h-8">
 				<div className="flex items-center">
 					<div className="flex mr-6 border rounded-md w-14 h-7">
-						<Btn className="flex items-center justify-center w-1/2 h-full p-0 border-0 hover:bg-gray-100" onClick={() => changeWeek(-7)}>
+						<Btn
+							className="flex items-center justify-center w-1/2 h-full p-0 border-0 hover:bg-gray-100"
+							onClick={() => changeWeek(-7)}
+						>
 							<ChevronLeft size={20} color="#B5B5B5" />
 						</Btn>
-						<Btn className="flex items-center justify-center w-1/2 h-full p-0 border-0 hover:bg-gray-100" onClick={() => changeWeek(7)}>
+						<Btn
+							className="flex items-center justify-center w-1/2 h-full p-0 border-0 hover:bg-gray-100"
+							onClick={() => changeWeek(7)}
+						>
 							<ChevronRight size={20} color="#B5B5B5" />
 						</Btn>
 					</div>
 
 					<div className="flex items-center">
-						<h2 className="mr-1 text-xl">2022 1월</h2>
+						<h2 className="mr-1 text-xl">2022 12월</h2>
 						<ChevronDown size={20} color="#B5B5B5" />
 					</div>
 				</div>
 				<div className="flex items-center justify-between h-full">
 					{onWeek && (
-						<Btn size="small" type="dashed" className="flex items-center mr-4 rounded-md" onClick={weekToggle}>
+						<Btn
+							size="small"
+							type="dashed"
+							className="flex items-center mr-4 rounded-md"
+							onClick={weekToggle}
+						>
 							<span className="mx-1 text-xs text-[#1CE6D5]">이번주</span>
 							<X size={14} color="#B5B5B5" />
 						</Btn>
@@ -54,7 +65,11 @@ const RoutineNav = ({ changeWeek, onAdd, routineToggle, ActiveStatusTolggle }: I
 						<Btn onClick={routineToggle}>루틴 추가하기</Btn>
 						{onAdd && <RoutineEditor routineToggle={routineToggle} onAdd={onAdd} />}
 					</div>
-					<DropDown ActiveStatusTolggle={ActiveStatusTolggle} onWeek={onWeek} setOnWeek={setOnWeek}></DropDown>
+					<DropDown
+						ActiveStatusTolggle={ActiveStatusTolggle}
+						onWeek={onWeek}
+						setOnWeek={setOnWeek}
+					></DropDown>
 				</div>
 			</div>
 		</div>
